6c6218a154a84ff0e22b31450440d090ea0eb079
kalimel <kalimel@mail.ru>
2019-06-14 21:41:38 +0300

    task 18

---
+++ addressbook_tests_autoit/appmanager/GroupHelper.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addressbook_tests_autoit
+{
+    public class GroupHelper : HelperBase
+    {
+        public static string GROUPWINTITLE = "Group editor";
+        public GroupHelper(ApplicationManager manager) : base(manager) { }
+
+        public List<GroupData> GetGroupList()
+        {
+            List<GroupData> list = new List<GroupData>();
+            OpenGroupsDialogue();
+            string count = aux.ControlTreeView(GROUPWINTITLE, "", "WindowsForms10.SysTreeView32.app.0.2c908d51", "GetItemCount", "#0", "");
+            for (int i = 0; i < int.Parse(count); i++)
+            {
+                string item = aux.ControlTreeView(GROUPWINTITLE, "", "WindowsForms10.SysTreeView32.app.0.2c908d51", "GetText", "#0|#" + i, "");
+                list.Add(new GroupData()
+                {
+                    Name = item
+                });
+            }
+            CloseGroupsDialogue();
+            return list;
+        }
+
+        public void Remove(GroupData groupToRemove)
+        {
+            OpenGroupsDialogue();
+            int index = FindGroupIdxByName(groupToRemove.Name);
+
+            if (index == -1)
+            {
+                throw new Exception("Group not found in group list: " + groupToRemove.Name);
+            }
+
+            SelectGroupByIndex(index);
+            aux.ControlClick(GROUPWINTITLE, "", "WindowsForms10.BUTTON.app.0.2c908d51");    // Delete
+            aux.ControlClick(GROUPWINTITLE, "", "WindowsForms10.BUTTON.app.0.2c908d52");    // Radio 
+            aux.Send("{Enter}");    // Ok
+            CloseGroupsDialogue();
+        }
+
+        public void Add(GroupData newGroup)
+        {
+            OpenGroupsDialogue();
+            aux.ControlClick(GROUPWINTITLE, "", "WindowsForms10.BUTTON.app.0.2c908d53");
+            aux.Send(newGroup.Name);
+            aux.Send("{Enter}");
+            CloseGroupsDialogue();
+        }
+
+        public void OpenGroupsDialogue()
+        {
+            aux.ControlClick(WINTITLE, "", "WindowsForms10.BUTTON.app.0.2c908d512");
+            aux.WinWait(GROUPWINTITLE);
+        }
+
+        public void CloseGroupsDialogue()
+        {
+            aux.ControlClick(GROUPWINTITLE, "", "WindowsForms10.BUTTON.app.0.2c908d54");
+        }
+
+        public void SelectGroupByIndex(int index)
+        {
+            aux.ControlTreeView(GROUPWINTITLE, "", "WindowsForms10.SysTreeView32.app.0.2c908d51", "Select", "#0|#" + index, "");
+        }
+
+        public int FindGroupIdxByName(string groupName)
+        {
+            string count = aux.ControlTreeView(GROUPWINTITLE, "", "WindowsForms10.SysTreeView32.app.0.2c908d51", "GetItemCount", "#0", "");
+            for (int i = 0; i < int.Parse(count); i++)
+            {
+                string name = aux.ControlTreeView(GROUPWINTITLE, "", "WindowsForms10.SysTreeView32.app.0.2c908d51", "GetText", "#0|#" + i, "");
+                if (name == groupName)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ addressbook_tests_autoit/tests/GroupRemovalTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace addressbook_tests_autoit
+{
+    [TestFixture]
+    public class GroupRemovalTests : TestBase
+    {
+        [Test]
+        public void TestGroupRemoval()
+        {
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+
+            if (oldGroups.Count == 1)
+            {
+                Random r = new Random();
+                GroupData newGroup = new GroupData()
+                {
+                    Name = "newwgrouppp" + r.Next(1, 9999)
+                };
+
+                app.Groups.Add(newGroup);
+                oldGroups.Add(newGroup);
+            }
+
+            GroupData groupToRemove = oldGroups.First();
+            app.Groups.Remove(groupToRemove);
+
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+            oldGroups.Remove(groupToRemove);
+            oldGroups.Sort();
+            newGroups.Sort();
+
+            Assert.AreEqual(oldGroups, newGroups);
+        }
+    }
+}
